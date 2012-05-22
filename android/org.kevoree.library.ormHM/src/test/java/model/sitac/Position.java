eae543963d3b060be38e292bbb360799e0636a53
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-05-22 15:07:59 +0200

    clean libraries by removing the ones hosted on kevoree-core fix pom

--- android/org.kevoree.library.ormHM/src/test/java/model/sitac/Position.java
+++
@@ -1,8 +0,0 @@
-package model.sitac;
-
-public abstract class Position {
-	public abstract double getLat();
-	public abstract double getLong();
-	public abstract void setLat(double lat);
-	public abstract void setLong(double lon);
-}
